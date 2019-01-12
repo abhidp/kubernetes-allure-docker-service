a32db95149f947701460d35b5d4fbd50c0a81bc3
Abhi Daspatnaik <abhijeet.daspatnaik@massive.co>
2019-01-12 18:56:26 +1100

    removedCopyhistory

--- scripts/generateAllureReport.sh
+++ scripts/generateAllureReport.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo $(TZ=$TIMEZONE date) " Generating new report"
-cp -R allure-report/history/* allure-results/history/
+# cp -R allure-report/history/* allure-results/history/
 allure generate --clean
 echo $(TZ=$TIMEZONE date) " New Report Available on port:$PORT"
